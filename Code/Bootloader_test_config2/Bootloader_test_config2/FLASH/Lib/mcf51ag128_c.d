92cfa084abbe133d1158e9b38b6dbeb6e001eb3a
Gautier Jobert <60614318+GautierDev31@users.noreply.github.com>
2021-03-26 15:47:48 +0100

    Maj Code & Scripts

--- Code/Bootloader_test_config2/Bootloader_test_config2/FLASH/Lib/mcf51ag128_c.d
+++
@@ -1,6 +0,0 @@
-Lib/mcf51ag128_c.obj: C:\Users\IRAP_CTA\Desktop\Gautier\programme\ IB\Bootloader_test_config2\Bootloader_test_config2\Lib\mcf51ag128.c \
-	C:\Freescale\CW\ MCU\ v10.7\MCU\ColdFire_Support\ewl\EWL_C\include\lib_ewl.prefix \
-	C:\Freescale\CW\ MCU\ v10.7\MCU\ColdFire_Support\ewl\EWL_C\include\common.prefix \
-	C:\Freescale\CW\ MCU\ v10.7\MCU\ColdFire_Support\ewl\EWL_C\include\ewl_c_version.h \
-	C:\Users\IRAP_CTA\Desktop\Gautier\programme\ IB\Bootloader_test_config2\Bootloader_test_config2\Project_Headers\porting_support.h \
-	C:\Users\IRAP_CTA\Desktop\Gautier\programme\ IB\Bootloader_test_config2\Bootloader_test_config2\Project_Headers\mcf51ag128.h 
--- Scripts_Python/Spi.py
+++ Scripts_Python/Spi.py
@@ -20,6 +20,15 @@ def SPI_send_return_input(input, last_send):
         print("Value received :",Value)  
         print ("---------")
         
+        
+def SPI_send(input):
+    msb = input >> 8
+    lsb = input & 0xFF
+    response = spi.xfer([msb, lsb])
+    Value = response[1] + (response[0] << 8);
+    print("Value sended : ", input)
+    print("Response :", Value)
+        
 def SPI_check_memory(length):
     Errors_check_memory = 0
     cmd = 81
@@ -29,7 +38,7 @@ def SPI_check_memory(length):
     
     for i in range (length):
         if (i < 2):
-            cmd = 80
+            cmd = 50
             msb = cmd >> 8
             lsb = cmd & 0xFF
             response = spi.xfer([msb, lsb])
@@ -37,7 +46,7 @@ def SPI_check_memory(length):
             value_from_spi.append(Value)
             
         else :
-            cmd = 80
+            cmd = 100
             msb = cmd >> 8
             lsb = cmd & 0xFF
             response = spi.xfer([msb, lsb])
@@ -49,6 +58,7 @@ def SPI_check_memory(length):
             # Comparaison
             if (Brut_memory_int != value_from_spi[i]):
                 print("Error")
+                print("ID :", i)
                 print("Expectect value :", Brut_memory_int)
                 print("Value received :",value_from_spi[i])            
                 print("----")
@@ -64,17 +74,53 @@ def SPI_check_memory(length):
     Value = response[1] + (response[0] << 8);
     print("End check memory")
 
-
+def SPI_write_memory(length):
     
-SPI_check_memory(222)
+    cmd = 80
+    msb = cmd >> 8
+    lsb = cmd & 0xFF
+    response = spi.xfer([msb, lsb])
+    
+    cmd = length
+    msb = cmd >> 8
+    lsb = cmd & 0xFF
+    response = spi.xfer([msb, lsb])
+    
+    for i in range (length):
+            Brut_memory = f.read(4)
+            Brut_memory_int = int(Brut_memory, 16)
+            cmd = Brut_memory_int
+            msb = cmd >> 8
+            lsb = cmd & 0xFF
+            response = spi.xfer([msb, lsb])
+            Value = response[1] + (response[0] << 8);
+            value_from_spi.append(Value)
+            print("Value sended : ", cmd)
+            print("Response :", Value)
+            time.sleep(0.1)
+    cmd = 81
+    msb = cmd >> 8
+    lsb = cmd & 0xFF
+    response = spi.xfer([msb, lsb])
+            
+    
 
+#SPI_send(50)
 
+SPI_write_memory(468)
+#time.sleep(1)
+#SPI_send(81)
+
+#SPI_check_memory(908)
+
+SPI_send(120)
 """
 while i != 101 :
     word_hex = f.read(4)
     word_int = int(word_hex, 16)
     value.append(word_int)
-    SPI_send(value[i], value[i-1])
+    SPI_send(value[i
+    ], value[i-1])
     time.sleep(0.01)
     i += 1
 
@@ -85,4 +131,5 @@ while i != 101 :
     print (value[i])
     i += 1
 """
+
 
